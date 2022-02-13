ca41782a4cdc64e816cfd26e2c9e7e30d4e9a2bf
mrescorcia <deg40@outlook.com>
2022-02-13 11:10:05 -0500

    Agregar archivos de proyecto.

---
+++ Data/WebAPIEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIEF.Entities;
+
+namespace WebAPIEF.Data
+{
+    public class WebAPIEFContext : DbContext
+    {
+        public WebAPIEFContext (DbContextOptions<WebAPIEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIEF.Entities.User> User { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIEF.Entities
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string userEmail { get; set; }
+        public string userPwd { get; set; }
+    }
+}
